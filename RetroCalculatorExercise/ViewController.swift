43ab93684b039673f867e6eb750bf68e39c91fd1
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-12 08:04:42 -0500

    Add IBOutlet for label and custom front

--- RetroCalculatorExercise/ViewController.swift
+++ RetroCalculatorExercise/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLBL: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        outputLBL.text = "0"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
